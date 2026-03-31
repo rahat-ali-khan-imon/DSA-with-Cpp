465888975c82bf022c34731889b9c5711875a76c
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-01 00:44:12 +0600

    Add factorial program in C++

--- Cpp/Basics/p1_factorial.cpp
+++ Cpp/Basics/p1_factorial.cpp
@@ -3,3 +3,20 @@ n! = n(n - 1)(n - 2)(n - 3).......3 * 2 * 1
 5! = 5 * 4 * 3 * 2 * 1
 */
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int fact = 1;
+
+    for(int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    cout << fact << endl;
+
+    return 0;
+}
