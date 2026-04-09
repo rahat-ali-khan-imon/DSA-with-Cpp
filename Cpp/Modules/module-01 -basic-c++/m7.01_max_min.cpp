59223a83d5a7b58391353cb750d539318ebe84f1
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-09 15:37:33 +0600

    Clean examples of min, max, and swap functions

--- Cpp/Modules/module-01 -basic-c++/m7.01_max_min.cpp
+++ Cpp/Modules/module-01 -basic-c++/m7.01_max_min.cpp
@@ -1,35 +1,3 @@
-/* ✅ Summary Table
-| Type           | `max()` / `min()`     | `swap()` |
-| -------------- | --------------------- | -------- |
-| `int`          | ✅                     | ✅        |
-| `char`         | ✅                     | ✅        |
-| `float/double` | ✅                     | ✅        |
-| `string`       | ✅ (lexical)           | ✅        |
-| `vector`       | ❌ (need max\_element) | ✅        |
-| `pair`         | ✅ (lexical compare)   | ✅        |
-*/
-
-#include<iostream>
-#include<algorithm>
-
-using namespace std;
-
-int main() {
-    int num1, num2;
-    cout << "Enter any two number: ";
-    cin >> num1 >> num2;
-
-    cout << "Minimum number: " << min(num1, num2) << endl;
-    cout << "Maximum number: " << max(num1, num2) << endl;
-
-    swap(num1, num2);
-
-    cout << "Swaping number: " << num1 << "\t" << num2 << endl;
-
-    return 0;
-}
-
-/*
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -37,37 +5,12 @@ int main() {
     int n1, n2;
     cin >> n1 >> n2;
 
-    cout << min(n1, n2) << " " << max(n1, n2) << endl;
+    cout << "Minimum number: " << min(n1, n2) << endl;
+    cout << "Maximum number: " << max(n1, n2) << endl;
 
     swap(n1, n2);
 
-    cout << n1 << " " << n2 << endl;
+    cout << "Swapping number: " << n1 << "\t" << n2 << endl;
 
     return 0;
 }
-
-
-##>-----------------------------------
-
-
-#include<bits/stdc++.h>
-
-using namespace std;
-
-int main() {
-    int num1, num2;
-    cout << "Enter any two number: ";
-    cin >> num1 >> num2;
-
-    int mx_val = max(num1, num2);
-
-    cout << mx_val << endl;
-    cout << min(num1, num2) << endl;
-
-    swap(num1, num2);
-
-    cout << num1 << "\t" << num2 << endl;
-
-    return 0;
-}
-*/
