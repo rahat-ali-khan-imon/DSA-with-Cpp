f486e0f0bc98ee75f87077d4a6268818d66929a0
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-13 01:44:28 +0600

    Add shifted decreasing pyramid patterns using numbers, binary, alphabets, and stars

---
+++ Cpp/Patterns/p5.02_binaty_pyramid.cpp
@@ -0,0 +1,28 @@
+/*
+1   0   1   0   1   
+    1   0   1   0   
+        1   0   1   
+            1   0
+                1
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = n; i >= 1; i--) {
+        for(int j = 1; j <= n - i; j++) {
+            cout << "  ";
+        }
+        for(int j = 1; j <= i; j++) {
+            cout << j % 2 << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
