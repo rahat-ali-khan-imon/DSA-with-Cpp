1e002ff42e04d6425ec51deaad3a8d4d2379a3ab
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-13 00:50:46 +0600

    Add right-aligned pyramid patterns: numbers, alphabets, and stars

---
+++ Cpp/Patterns/p4.02_alphabet_pyramid.cpp
@@ -0,0 +1,27 @@
+/*
+                A   
+            A   B   
+        A   B   C   
+    A   B   C   D
+A   B   C   D   E
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n - i; j++) {
+            cout << "  ";                       // 2 space
+        }
+        for(int j = 1; j <= i; j++) {
+            cout << char(j + 64) << " ";                 // 1 space
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ Cpp/Patterns/p4.03_star_pyramid.cpp
@@ -0,0 +1,27 @@
+/*
+        * 
+      * * 
+    * * * 
+  * * * * 
+* * * * * 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n - i; j++) {
+            cout << "  ";                       // 2 space
+        }
+        for(int j = 1; j <= i; j++) {
+            cout << "*" << " ";                 // 1 space
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
