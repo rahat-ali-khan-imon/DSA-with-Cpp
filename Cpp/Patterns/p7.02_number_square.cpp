905ac7d714acda687290bdc26af945a3ce53296a
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-14 21:11:01 +0600

    Add square grid patterns using numbers and stars

---
+++ Cpp/Patterns/p7.02_number_square.cpp
@@ -0,0 +1,23 @@
+/*
+1  1  1  1  
+2  2  2  2  
+3  3  3  3  
+4  4  4  4 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            cout << i << "  ";              // 2 space
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
