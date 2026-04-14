c901f107e9338d6185e9081a0bf0eaa6620bcd73
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-14 21:00:03 +0600

    Add diamond pattern series using numbers, alphabets, and stars

---
+++ Cpp/Patterns/p6.04_star_diamond.cpp
@@ -0,0 +1,39 @@
+/*
+      * 
+    * * 
+  * * * 
+* * * * 
+  * * * 
+    * * 
+      * 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n - i; j++) {
+            cout << "  ";                      // 2 space
+        }
+        for(int j = 1; j <= i; j++) {
+            cout << "*" << " ";               // 1 space
+        }
+        cout << endl;
+    }
+
+    for(int i = n - 1; i >= 1; i--) {
+        for(int j = 1; j <= n - i; j++) {
+            cout << "  ";                       // 2 space
+        }
+        for(int j = 1; j <= i; j++) {
+            cout << "*" << " ";                 // 1 space
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
