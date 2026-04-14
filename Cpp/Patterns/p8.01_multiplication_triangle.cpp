15732d69848c5824cb19f0e3a237555f70b1e27e
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-14 21:16:51 +0600

    Add multiplication-based number triangle pattern

---
+++ Cpp/Patterns/p8.01_multiplication_triangle.cpp
@@ -0,0 +1,23 @@
+/*
+1  
+2  4  
+3  6  9  
+4  8  12  16 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            cout << i * j << "  ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
