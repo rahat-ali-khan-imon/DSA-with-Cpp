4b6f1888f541dab77e3bd70f63b3d2793534e91d
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-04-20 10:46:49 +0600

    Add constructor and display method in Student class

---
+++ Cpp/Modules/module-03-class-and-object/m05.01_constructor.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student {
+    public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int r, int c, double g) {   // Constructor declare
+        roll = r;
+        cls = c;
+        gpa = g;
+    }
+};
+
+int main() {
+    Student rahim(45, 5, 3.9);      // Constructor call
+    Student karim(234, 4, 3.99);
+
+    cout << rahim.roll << endl;
+    cout << rahim.cls << endl;
+    cout << rahim.gpa << endl;
+
+    cout << endl;
+
+    cout << karim.roll << endl;
+    cout << karim.cls << endl;
+    cout << karim.gpa << endl;
+
+    return 0;
+}
---
+++ Cpp/Modules/module-03-class-and-object/m05.02.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student {
+    public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int r, int c, double g) {
+        roll = r;
+        cls = c;
+        gpa = g;
+    }
+
+    void display() {
+        cout << "Roll: " << roll << endl;
+        cout << "Class: " << cls << endl;
+        cout << "GPA: " << gpa << endl;
+        cout << endl;
+    }
+};
+
+int main() {
+    Student rahim(45, 5, 3.9);
+    Student karim(234, 4, 3.99);
+
+    rahim.display();
+    karim.display();
+
+    return 0;
+}
