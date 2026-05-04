ef1b6339dd440df519c8c41af3b74b9f92117e5b
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-05 01:47:49 +0600

    Demonstrate string size and capacity functions in C++

---
+++ Cpp/Modules/module-05-string/m02.01_string_capacity.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "Hello World!";
+
+    cout << "String length or size: " << s.size() << endl;
+    cout << "String length: " << s.length() << endl;
+    cout << "Maximum size: " << s.max_size() << endl;
+    cout << "Capacity: " << s.capacity() << endl;
+
+    return 0;
+}
