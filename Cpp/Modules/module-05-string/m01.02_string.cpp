f0e6b5a553871daa951c1e221a6040f89fe6c577
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-04 19:45:44 +0600

    Demonstrate modifying C string using strcpy()

---
+++ Cpp/Modules/module-05-string/m01.02_string.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char str[10] = "Hello";
+    // str = "Gello";       // Ei vabe initialize kra jabe na
+    strcpy(str, "Gello");
+
+    cout << str << endl;
+
+    return 0;
+};
