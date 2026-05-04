21a98ad30e54677d2845aa823ce00390f9aa0a17
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-04 20:05:20 +0600

    Demonstrate string concatenation using + operator in C++

---
+++ Cpp/Modules/module-05-string/m01.06_string.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1 = "Hello";
+    string s2 = "world!";
+
+    string s = s1 + " " + s2;
+
+    cout << s << endl;
+
+    return 0;
+}
