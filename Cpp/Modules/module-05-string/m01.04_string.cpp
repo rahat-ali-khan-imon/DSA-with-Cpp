1af2bd838f3e35217ba06f30f066f9215328858d
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-04 19:59:49 +0600

    Demonstrate string comparison using == operator in C++

---
+++ Cpp/Modules/module-05-string/m01.04_string.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1 = "Hello";
+    string s2 = "Gello";
+
+    if(s1 == s2) {          // s1 == s2 character-by-character comparison.
+        cout << "Same" << endl;
+    } else {
+        cout << "Not Same" << endl;
+    }
+
+    return 0;
+}
