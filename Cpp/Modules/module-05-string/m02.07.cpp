3d1899e0c3091048383a874c61aabb5c6ca74b8f
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-05 04:14:17 +0600

    Improve comments for string resize() behavior explanation

--- Cpp/Modules/module-05-string/m02.07.cpp
+++ Cpp/Modules/module-05-string/m02.07.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
     string s = "Hi, Welcome";
 
-    s.resize(25);   // Hi, Welcome
+    s.resize(25);   // Hi, Welcome    Size: 25
     cout << "String resize s: " << s << "\tSize: " << s.size() << endl;
-    s.resize(20, 'A');  // Hi, Welcome
+    s.resize(20, 'A');  // Hi, Welcome    Size: 20
     cout << "String resize s: " << s << "\tSize: " << s.size() << endl;
-    s.resize(30, 'D');  // Hi, WelcomeDDDDDDDDDD
+    s.resize(30, 'D');  // Hi, WelcomeDDDDDDDDDD  Size: 30
     cout << "String resize s: " << s << "\tSize: " << s.size() << endl;
 
     return 0;
