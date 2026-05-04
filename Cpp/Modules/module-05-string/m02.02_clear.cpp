674ff1ec265b17703f64a432d24e0b6b73f7f1e8
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-05 02:04:26 +0600

    Demonstrate clearing a string using clear() in C++

---
+++ Cpp/Modules/module-05-string/m02.02_clear.cpp
@@ -0,0 +1,15 @@
+// String s.clear()
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "Hello World!";
+
+    s.clear();
+
+    cout << "String clear: " << s << endl;
+    cout << "String size s.clear(): " << s.size() << endl;
+
+    return 0;
+}
