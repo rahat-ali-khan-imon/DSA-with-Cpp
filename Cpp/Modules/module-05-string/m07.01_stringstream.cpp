dcdbd2d42dfdcb1ee318608fbe541e5df0fb950c
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-05 22:40:01 +0600

    Read full line input using getline for string processing

---
+++ Cpp/Modules/module-05-string/m07.01_stringstream.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    cout << s << endl;
+
+    return 0;
+}
+
+/*
+Input:
+Hello I am Mahmud Hossain Pias
+
+Output:
+Hello I am Mahmud Hossain Pias
+*/
