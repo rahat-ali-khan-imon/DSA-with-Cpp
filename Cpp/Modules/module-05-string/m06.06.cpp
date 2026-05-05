113d8e91318e7a7e92211b5f8f092995bdb32a07
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-05 20:38:50 +0600

    Fix getline input issue using cin.ignore()

---
+++ Cpp/Modules/module-05-string/m06.06.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+
+    string s;
+    cin.ignore();
+    getline(cin, s);
+
+    cout << x << endl;
+    cout << s << endl;
+
+    return 0;
+}
+
+/* Input with space
+Input:
+10
+Hello World!
+
+
+Output:
+10
+Hello World!
+*/
