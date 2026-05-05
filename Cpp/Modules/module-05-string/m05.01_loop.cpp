780033a4917f8ebead7ca97d2cf0119585b968ca
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-05 18:59:42 +0600

    Iterate over string using for loop and indexing

---
+++ Cpp/Modules/module-05-string/m05.01_loop.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "Hello World!";
+
+    for(int i = 0; i < s.size(); i++) {
+        cout << s[i] << endl;
+    }
+
+    return 0;
+}
