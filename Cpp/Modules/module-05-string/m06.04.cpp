14179420d151c2c0af53bd371985694bd841aeb3
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-05 20:37:57 +0600

    Show getline issue after cin (newline problem)

---
+++ Cpp/Modules/module-05-string/m06.04.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+
+    string s;   
+    getline(cin, s);
+
+    cout << x << endl;
+    cout << s << endl;
+
+    return 0;
+}
+
+/* Input with space
+Input:
+10
+Hello World!
+
+
+Output:
+10
+
+*/
