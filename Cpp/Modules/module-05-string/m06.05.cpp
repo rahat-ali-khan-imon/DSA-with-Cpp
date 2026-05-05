58f4b1f06e2214fc8b938211c9de8f6c34208738
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-05 20:38:31 +0600

    Read integer and single word string using cin

---
+++ Cpp/Modules/module-05-string/m06.05.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+
+    string s;
+    cin >> s;
+
+    cout << x << endl;
+    cout << s << endl;
+
+    return 0;
+}
+
+/* Input without space
+Input:
+10
+Hello
+
+
+Output:
+10
+Hello
+*/
