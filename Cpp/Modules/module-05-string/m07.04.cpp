54393714bf9c6f4f5a36758c1ab92b2c8e4f0d3b
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-05 22:41:29 +0600

    Count words in string using stringstream

---
+++ Cpp/Modules/module-05-string/m07.04.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    cout << s << endl;
+
+    stringstream ss(s);
+    string word;
+    int cnt = 0;
+
+    while(ss >> word) {
+        cout << word << endl;
+        cnt++;
+    }
+
+    cout << cnt << endl;
+     
+    return 0;
+}
+
+/*
+Input:
+Hello I am Mahmud Hossain Pias
+
+Output:
+Hello I am Mahmud Hossain Pias
+Hello
+I
+am
+Mahmud
+Hossain
+6
+
+*/
