ac61d4d0af5490dca313b79272a43910f4439df8
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-05 20:36:57 +0600

    Demonstrate cin limitation with multi-word input

---
+++ Cpp/Modules/module-05-string/m06.02.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    cout << s << endl;
+
+    return 0;
+}
+
+/*
+Input: 
+Hello World
+
+Output:
+Hello   
+*/
