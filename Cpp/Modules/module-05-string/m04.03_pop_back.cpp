1f0cea8429c9df86e5723306a1f59043ac93e77b
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-05 14:38:27 +0600

    Demonstrate removing characters using pop_back() in string

---
+++ Cpp/Modules/module-05-string/m04.03_pop_back.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "Hello World!";
+
+    s.pop_back();
+    cout << "String remove: " << s << endl;
+
+    s.pop_back();
+    s.pop_back();
+    cout << "String remove: " << s << endl;
+
+    return 0;
+}
