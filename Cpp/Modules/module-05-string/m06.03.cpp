84c75a1bd4033ec43bfbf04bfb4ccd8c50761110
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-05 20:37:27 +0600

    Read full line input using getline()

---
+++ Cpp/Modules/module-05-string/m06.03.cpp
@@ -0,0 +1,21 @@
+// String input with spaces
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    cout << s << endl;
+
+    return 0;
+}
+
+/*
+Input: 
+Hello World
+
+Output:
+Hello World
+*/
