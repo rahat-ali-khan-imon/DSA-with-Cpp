3a79d32d84f0bfb9c131f1cff335e92b711939cd
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 12:42:00 +0600

    Read string input using cin without spaces

---
+++ C++-for-DSA/basics-00/p05.03_character_array.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    cout << str << endl;
+
+    return 0;
+}
+
+/*
+Input:
+Hi, Hello, Programming
+
+Output:
+Hi,
+
+*/
