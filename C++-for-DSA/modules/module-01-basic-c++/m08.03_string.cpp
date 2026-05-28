d62c466916a8f3a2aaa5c6be49a9337a2fca9fc2
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 06:45:49 +0600

    Compare cin and getline input behavior

---
+++ C++-for-DSA/modules/module-01-basic-c++/m08.03_string.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char str[100];
+    cin >> str;
+
+    cout << str << endl;
+
+
+    string s;
+    getline(cin, s);
+
+    cout << s << endl;
+
+    return 0;
+}
+
+/*
+Input:
+programming is fun
+
+Output:
+programming
+ is fun
+
+*/
