0b4ded442b47d00a33877abfbea8cef634af4c0b
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 07:02:53 +0600

    Format floating point output using setprecision and fixed

---
+++ C++-for-DSA/modules/module-01-basic-c++/m04.02_setprecision.cpp
@@ -0,0 +1,13 @@
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    double d = 3.14151617;
+
+    cout << d << endl;
+    cout << fixed << setprecision(7) << d << endl;
+
+    return 0;
+}
