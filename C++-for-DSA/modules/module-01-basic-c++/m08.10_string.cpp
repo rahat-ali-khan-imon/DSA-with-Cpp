ade80e5e4e7a3681a4b645379ebb07e1d243a7aa
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 06:56:02 +0600

    Fix input buffer issue using cin.ignore with getline in C++

---
+++ C++-for-DSA/modules/module-01-basic-c++/m08.10_string.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string s;
+    cin.ignore();
+    getline(cin, s);
+
+    cout << n << endl;
+    cout << s;
+
+    return 0;
+}
+
+
+/*
+Input:
+23
+programming is fun
+
+Output:
+23
+programming is fun
+*/
