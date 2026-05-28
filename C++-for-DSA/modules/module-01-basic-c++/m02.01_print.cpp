51a2a37e9bb738c4f650c244446199f866fa21d3
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 06:58:01 +0600

    Basic C++ output with variables and std::cout

---
+++ C++-for-DSA/modules/module-01-basic-c++/m02.01_print.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+
+    int x = 10;
+    char ch = 'A';
+    double d = 123.34;
+
+    std::cout << x << " " << ch << std::endl << d;
+
+    return 0;
+}
