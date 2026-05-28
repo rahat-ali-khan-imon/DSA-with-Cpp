edd48ec61d44e47fabfb05faec8fb2a40f4e9c80
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 12:20:41 +0600

    Escape sequence examples in C++

---
+++ C++-for-DSA/basics-00/p02.01_escape-sequence.cpp
@@ -0,0 +1,13 @@
+//Escape sequence
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello \nWorld!" << endl;
+    cout << "Rahat Ali Khan Emon";
+    cout << endl;
+    cout << "\"Welcome\"" << endl;
+
+    return 0;
+}
