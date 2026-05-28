788926a0f70bf8abf6f34aa3474df2614a8abc33
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 06:53:49 +0600

    Handle input buffer issue using getchar before getline in C++

---
+++ C++-for-DSA/modules/module-01-basic-c++/m08.08_string.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char str[100];
+    int num;
+    cin >> num;
+    getchar();       // newline character (Enter key) input
+
+    
+    cin.getline(str, 100);
+
+    cout << num << endl;
+    cout << str << endl;
+
+    return 0;
+}
+
+
+/*
+Input:
+23
+programming is fun
+
+Output:
+23
+programming is fun
+
+*/
