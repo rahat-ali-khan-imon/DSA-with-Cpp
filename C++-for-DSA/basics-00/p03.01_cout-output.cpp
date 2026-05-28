df516ca12018801c6e19e7bf8d0681834727bb95
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 12:24:06 +0600

    Basic cout output formatting examples in C++

---
+++ C++-for-DSA/basics-00/p03.01_cout-output.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num1 = 10, num2 = 40;
+
+    cout << num1 << endl;
+    cout << num2 << endl;
+    cout << num1 << endl << num2 << endl;
+    cout << num1 << "\t" << num2 << endl;
+    cout << num1 << " " << num2 << endl;
+    cout << "500\t400" << endl;
+    cout << 700 << endl;
+    cout << 700 + 300;
+
+    return 0;
+}
