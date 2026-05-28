ce094a27fe137abb11db98c6ca08083c001b7688
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 06:50:41 +0600

    Read string input using cin (no spaces)

---
+++ C++-for-DSA/modules/module-01-basic-c++/m08.06_string.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+    // getline(cin, str);
+
+    cout << str << endl;
+
+    return 0;
+}
+
+/*
+Input:
+programming is fun
+
+Output:
+programming
+
+*/
