f84f8565400db691ee725db5020ac18eea60b71e
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 07:09:55 +0600

    Find minimum and maximum using initializer list with min and max

---
+++ C++-for-DSA/modules/module-01-basic-c++/m07.02_max_min.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+    cout << min({3, 5, 7, 10, 12}) << endl;
+    cout << max({23, 45, 678, 88, 90}) << endl;
+    cout << max({10, 20, 30, 40, 50}) << endl;
+    cout << min({50, 40, 30, 20, 10}) << endl;
+
+    return 0;
+}
