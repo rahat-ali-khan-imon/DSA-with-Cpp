344366dd193682c8d5d4610a61eb0bbd406f6442
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 06:49:14 +0600

    Fix input handling using cin.ignore with getline

---
+++ C++-for-DSA/modules/module-01-basic-c++/m08.05_string.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char str[100];
+    cin >> str;
+
+    cout << str << endl;
+
+
+    string s;
+    cin.ignore();
+    getline(cin, s);
+
+    cout << s << endl;
+
+    return 0;
+}
+
+/*
+Input:
+programming is fun
+
+Output:
+programming
+is fun
+
+*/
