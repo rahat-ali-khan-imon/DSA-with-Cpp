effd54dd80f8dac0e1391093c5cf199acf62071d
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 06:43:30 +0600

    Read full line input using getline in C++

---
+++ C++-for-DSA/modules/module-01-basic-c++/m08.02_string.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    cout << s << endl;
+
+    return 0;
+}
+
+/*
+Input:
+programming is fun
+
+Output:
+programming is fun
+
+*/
