7ac8e68a89ef16901e426858c1f0172ee14b609e
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 12:29:28 +0600

    Check size of different data types using sizeof

---
+++ C++-for-DSA/basics-00/p04.01_sizeof-datatype.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "Size of integer data type : " << sizeof(int) << " byte" << endl;
+    cout << "Size of floating data type : " << sizeof(float) << " byte" << endl;
+    cout << "Size of double data type : " << sizeof(double) << " byte" << endl;
+    cout << "Size of character data type : " << sizeof(char) << " byte" << endl;
+
+    return 0;
+}
