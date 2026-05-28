6c6f01af8f184295167d8ca340beb1f750fc0b00
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 12:17:24 +0600

    Basic C++ output formatting and escape sequence examples

---
+++ C++-for-DSA/basics-00/p01.02_print.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main() {
+    cout << "Rahat " << "Ali " << "Khan " << "Emon" << endl;
+    getch();
+
+    cout << "Hello World!" << endl;
+    cout << "Rahat Ali Khan Emon";
+    cout << endl;
+    cout << "\"Welcome\"" << endl;
+
+    return 0;
+}
