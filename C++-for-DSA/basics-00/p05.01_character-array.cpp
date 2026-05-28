ed2d625b0d2172d71b5a35d7f60b74fac4c4912a
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 12:36:47 +0600

    Character array string example in C++

---
+++ C++-for-DSA/basics-00/p05.01_character-array.cpp
@@ -0,0 +1,13 @@
+//Use of character type array
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    char str[20] = "Rahat Ali Khan Emon";
+
+    cout << "My name is "<< str << endl;
+    
+    return 0;
+}
