fff952ab5bcdd87af6a368b30dcc4c2a69c2ae6b
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-05-28 12:50:25 +0600

    Take integer and character input using cin in C++

---
+++ C++-for-DSA/basics-00/p06.01_user-input.cpp
@@ -0,0 +1,22 @@
+//How to get user input
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{   // number input
+    int num;
+    cin >> num;
+
+    cout << num << endl;
+    getch();
+    cout << endl;
+
+    // only one character input
+    char ch;
+    cin >> ch;
+
+    cout << ch << endl;
+    return 0;
+}
